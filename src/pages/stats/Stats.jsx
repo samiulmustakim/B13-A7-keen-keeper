c1bc6dc8d4da5aa42955f31f68de11afefd51df7
samiulmustakim <samiulmustakim99@gmail.com>
2026-04-17 08:47:35 +0600

    Added Stats

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { FriendContactContext } from "../../contextApi/FriendContactContext";
 
 const Stats = () => {
+    const { timeline } = useContext(FriendContactContext);
+
+    const data = [
+        {
+            name: "Text",
+            value: timeline.filter((item) => item.type === "message").length,
+            fill: "#7f37f5",
+        },
+        {
+            name: "Call",
+            value: timeline.filter((item) => item.type === "call").length,
+            fill: "#244d3f",
+        },
+        {
+            name: "Video",
+            value: timeline.filter((item) => item.type === "video").length,
+            fill: "#37a163",
+        },
+    ];
+
     return (
-        <div>
-            
+        <div className="bg-gray-100 py-20 px-4">
+            <div className="container mx-auto pb-10">
+                <h1 className="text-4xl font-bold">Friendship Analytics</h1>
+            </div>
+            <div className="container mx-auto bg-white shadow-md text-xl p-7">
+                <h1 className="font-semibold">By Interaction Type</h1>
+                <div className="flex justify-center items-center py-15">
+                    <ResponsiveContainer width="100%" height={400}>
+                        <PieChart>
+                            <Pie
+                                data={data}
+                                innerRadius="60%"
+                                outerRadius="80%"
+                                cornerRadius="50%"
+                                paddingAngle={5}
+                                dataKey="value"
+                                isAnimationActive={true}
+                            />
+                            <Tooltip />
+                            <Legend wrapperStyle={{ padding: "20px" }} />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -4,6 +4,7 @@ import callimg from "../../assets/Call.png";
 import messageimg from "../../assets/Message.png";
 import videoimg from "../../assets/video.png";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { CgChevronDoubleRightO } from "react-icons/cg";
 
 const Timeline = () => {
     const { timeline } = useContext(FriendContactContext);
@@ -21,57 +22,81 @@ const Timeline = () => {
             : timeline.filter((item) => item.type === filter);
 
     return (
-        <div className="container mx-auto py-14 space-y-4 px-4">
-            <h1 className="text-5xl font-bold text-[#244d3f]">Timeline </h1>
+        <div className="bg-gray-100">
+            <div className="container mx-auto py-14 space-y-4 px-4">
+                <h1 className="text-5xl font-bold text-[#244d3f]">Timeline </h1>
 
-            <div className="dropdown dropdown-bottom pb-3">
-                <div
-                    tabIndex={0}
-                    role="button"
-                    className="btn m-1 justify-start w-80 flex items-center text-gray-500"
-                >
-                    {filter === "all" ? "Filter timeline" : typeConfig[filter].label} <IoMdArrowDropdown />
+                <div className="dropdown dropdown-bottom pb-3">
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        className="btn m-1 justify-start w-80 flex items-center text-gray-500"
+                    >
+                        {filter === "all"
+                            ? "Filter timeline"
+                            : typeConfig[filter].label}{" "}
+                        <IoMdArrowDropdown />
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+                    >
+                        <li>
+                            <a onClick={() => setFilter("all")}>All</a>
+                        </li>
+                        <li>
+                            <a onClick={() => setFilter("message")}>Text</a>
+                        </li>
+                        <li>
+                            <a onClick={() => setFilter("call")}>Call</a>
+                        </li>
+                        <li>
+                            <a onClick={() => setFilter("video")}>Video</a>
+                        </li>
+                    </ul>
                 </div>
-                <ul
-                    tabIndex="-1"
-                    className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-                >
-                    <li>
-                        <a onClick={() => setFilter("message")}>Text</a>
-                    </li>
-                    <li>
-                        <a onClick={() => setFilter("call")}>Call</a>
-                    </li>
-                    <li>
-                        <a onClick={() => setFilter("video")}>Video</a>
-                    </li>
-                </ul>
-            </div>
 
-            {/* call  */}
-            <div className="space-y-3">
-                {filterTimeline.map((item) => {
-                    const config = typeConfig[item.type];
-                    return (
-                        <div className="flex items-center gap-4 bg-white p-6 rounded-md shadow-sm">
-                            <img src={config.img} alt="" />
-                            <div>
-                                <h1 className="text-xl text-gray-600">
-                                    <span className="font-semibold text-gray-800">
-                                        {config.label}{" "}
-                                    </span>
-                                    with {item.name}
-                                </h1>
-                                <h1 className="text-gray-600">
-                                    Meetup with {item.next_due_date}
-                                </h1>
-                            </div>
+                {/* call  */}
+                {filterTimeline.length === 0 ? (
+                    <div className="flex justify-center items-center p-6 py-10 bg-gray-50 rounded-lg shadow-md">
+                        <div className="flex flex-col gap-3 justify-center items-center">
+                            <p>
+                                <CgChevronDoubleRightO className="text-5xl text-gray-600" />
+                            </p>
+                            <p className="text-gray-600 text-xl font-semibold">
+                                No data added to timeline.
+                            </p>
+                            <p className="text-gray-400  text-sm">
+                                Looks like there is no activity yet. Start by
+                                adding a new entry.
+                            </p>
                         </div>
-                    );
-                })}
-            </div>
+                    </div>
+                ) : (
+                    <div className="space-y-3">
+                        {filterTimeline.map((item) => {
+                            const config = typeConfig[item.type];
+                            return (
+                                <div className="flex items-center gap-4 bg-white p-6 rounded-md shadow-sm">
+                                    <img src={config.img} alt="" />
+                                    <div>
+                                        <h1 className="text-xl text-gray-600">
+                                            <span className="font-semibold text-gray-800">
+                                                {config.label}{" "}
+                                            </span>
+                                            with {item.name}
+                                        </h1>
+                                        <h1 className="text-gray-600">
+                                            Meetup with {item.next_due_date}
+                                        </h1>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
 
-            {/* <div className="space-y-3">
+                {/* <div className="space-y-3">
                 {message.map((c) => {
                     return (
                         <div className="flex items-center gap-4 bg-white p-6 rounded-md shadow-sm">
@@ -98,6 +123,7 @@ const Timeline = () => {
                     );
                 })}
             </div> */}
+            </div>
         </div>
     );
 };
